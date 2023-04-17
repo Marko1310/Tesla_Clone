f0702b03fb6e59561b3bc7f10a7f092fdbcc5ab5
Marko1310 <marko.cabo13@gmail.com>
2023-04-17 23:29:33 +0200

    stylizing background and buttons

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,18 @@
-import './Section.css'
+import './Section.css';
 
 const Section = () => {
-    return ( <div className='section-container'>
-        Section
-    </div> );
-}
- 
+  return (
+    <div className="section-container">
+      <div className="section-text-container">
+        <h1 className="section-text-title">Model S</h1>
+        <p className="section-text-text">View Inventory</p>
+      </div>
+      <div className="button-container">
+        <button className="button-left">Order Now</button>
+        <button className="button-right">Demo Drive</button>
+      </div>
+    </div>
+  );
+};
+
 export default Section;
