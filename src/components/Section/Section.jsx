e107d03320e434e674dd65a071d75df1046b5125
Marko1310 <marko.cabo13@gmail.com>
2023-04-18 00:11:11 +0200

    stylizing buttons, arrow, animation

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -7,9 +7,12 @@ const Section = () => {
         <h1 className="section-text-title">Model S</h1>
         <p className="section-text-text">View Inventory</p>
       </div>
-      <div className="button-container">
-        <button className="button-left">Order Now</button>
-        <button className="button-right">Demo Drive</button>
+      <div className="section-bottom">
+        <div className="button-container">
+          <button className="button-left">Order Now</button>
+          <button className="button-right">Demo Drive</button>
+        </div>
+        <img src="./images/down-arrow.svg" className="arrow" alt="arrow"></img>
       </div>
     </div>
   );
