b94d81e8ebf633972d235a90428b4bc99473f9b3
Marko1310 <marko.cabo13@gmail.com>
2023-04-17 22:00:57 +0200

    building basic components, Header, Home

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,9 @@
+import './Home.css'
+import Section from "../Section/Section"
+
+
+const Home = () => {
+    return ( <div className="home-container"><Section/></div> );
+}
+ 
+export default Home;
---
+++ src/components/Section/Section.jsx
@@ -0,0 +1,9 @@
+import './Section.css'
+
+const Section = () => {
+    return ( <div className='section-container'>
+        Section
+    </div> );
+}
+ 
+export default Section;
