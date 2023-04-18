ce7c501d84eeae49e5a70fbea55d0ee77e208483
Marko1310 <marko.cabo13@gmail.com>
2023-04-19 00:27:33 +0200

    popmenu styling

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,13 @@
+import React, { useState } from 'react';
+
 import logo from '../../images/logo.svg';
+import x from '../../images/close-button.png';
 import './Header.css';
 import { inventory } from '../../data/inventory';
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className="header-container">
       <a>
@@ -23,6 +28,9 @@ const Header = () => {
         <a className="menu-right-tags menu">Menu</a>
       </div>
       <div className="popmenu">
+        <div className="x-container">
+          <img className="x" alt="x" src={x} />
+        </div>
         {inventory.map((el) => {
           return (
             <a className="popmenu-tags" key={el.id} href={`#${el.id}`}>
